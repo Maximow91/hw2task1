4c378aa8d7836eee7e8d9877bb654dd7996d73ee
Kirill Maximow <markizzzzka91@gmail.com>
2021-10-20 21:07:02 +0300

    src added

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+var commisionPercentage  = 0.0075
+var minCommision = 3500
+var commisionAmmount =0.0
+
+fun main(args: Array<String>) {
+
+    println("Введите сумму перевода в копейках:")
+    var ammount: Int = readLine()!!.toInt()
+    commisionAmmount = when { ammount * commisionPercentage < minCommision -> minCommision.toDouble() else -> ammount * commisionPercentage}
+
+     println("Cумма коммисии составит: ${commisionAmmount.toInt()} копеек")
+}
+
